acd1ecfaeae2fdf275c7bb4205be2b557f6135d1
stillmattwest <stillmattwest@gmail.com>
2018-09-15 21:35:32 -0700

    added mod1 self-assessment

---
+++ Mod1SA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod1SA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
