d175a9e12c626717defc14ced8d35c602f24c5a6
matt-codeslo <matt@codeslo.com>
2018-09-19 14:32:44 -0700

    Completed Module 3

---
+++ Module3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Module3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReaderObject = null;
+
+            try
+            {
+                streamReaderObject = new StreamReader("file1.txt");
+
+                string contents = streamReaderObject.ReadToEnd();
+
+                streamReaderObject.Close();
+
+                Console.WriteLine("The file has {0} text elements.", new StringInfo(contents).LengthInTextElements);
+            }
+            catch
+            {
+                Console.WriteLine("The file cannot be found.");
+            }
+            finally
+            {
+                if(streamReaderObject != null){
+                    streamReaderObject.Dispose();
+                }
+            }
+        }
+    }
+}
